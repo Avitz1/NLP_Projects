b8e4e0fe04a4852d860f7c91c3f99151e2d17cf2
Avi <1avilifshitz@gmail.com>
2022-11-10 21:28:14 +0200

    Initial commit. A main file and wikipedia texts for future analysis

---
+++ main.py
@@ -0,0 +1,10 @@
+import json
+from sys import argv
+
+# Define your classes here #
+if __name__ == "__main__":
+
+    corpus_dir = argv[1]    # The directory in which the Wiki files are, full pathname
+    output_file = argv[2]   # The text file that the corpus is written onto, full pathname
+    # Implement the program here #
+
